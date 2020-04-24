edfabd17b891ee4517a83dc8d29aa758d9660e7a
a77321a <1817169988@qq.com>
2020-04-24 18:06:11 +0800

    ashu

--- express-vue-demo/server/apiRoutes/home/index.js
+++ express-vue-demo/server/apiRoutes/home/index.js
@@ -3,12 +3,13 @@
  * @Author: 
  * @Date: 2019-12-29 14:37:37
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-04-18 16:31:27
+ * @LastEditTime: 2020-04-24 18:06:06
  */
 import router from '../../index'
 
 router.get('/eeeor', (req, res, next) => {
     console.log(1111)
+    req.setEncoding()
 })
 
 module.exports = router
