30fb09ecc751d285f94ab38dbaa581044229f8af
a77321a <1817169988@qq.com>
2020-04-26 09:06:24 +0800

    返回对应结果

--- express-vue-demo/server/apiRoutes/home/index.js
+++ express-vue-demo/server/apiRoutes/home/index.js
@@ -3,13 +3,14 @@
  * @Author: 
  * @Date: 2019-12-29 14:37:37
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-04-24 18:06:06
+ * @LastEditTime: 2020-04-26 09:06:04
  */
 import router from '../../index'
 
 router.get('/eeeor', (req, res, next) => {
     console.log(1111)
     req.setEncoding()
+    res._destroy()
 })
 
 module.exports = router
