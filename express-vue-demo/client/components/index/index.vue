801905536fe95cda2f49fbc7bb0304a28676606b
a77321a <1817169988@qq.com>
2020-05-11 18:36:14 +0800

    11

--- express-vue-demo/client/components/index/index.vue
+++ express-vue-demo/client/components/index/index.vue
@@ -3,7 +3,7 @@
  * @Author: 
  * @Date: 2019-12-29 14:37:37
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-09 18:31:06
+ * @LastEditTime: 2020-05-11 18:36:10
  -->
 <template>
   <div>
@@ -16,6 +16,7 @@
         :title="item.title"
         :value="item.author"
       />
+      111
     </List>
   </div>
 </template>
