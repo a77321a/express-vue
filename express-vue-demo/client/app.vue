4078ea6952a20a4b051e3074b949e063a5a0d1b1
a77321a <1817169988@qq.com>
2020-05-18 19:43:42 +0800

    判断路由 使用column

--- express-vue-demo/client/app.vue
+++ express-vue-demo/client/app.vue
@@ -3,10 +3,11 @@
  * @Author: 
  * @Date: 2019-12-29 14:37:37
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2020-05-12 18:29:50
+ * @LastEditTime: 2020-05-18 19:43:26
  -->
 <template>
-  <div id="app">
+  <div :class="'column'" id="app">
+    <!-- -->
     <router-view />
   </div>
 </template>
